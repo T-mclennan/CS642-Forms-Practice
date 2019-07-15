26e9a7ee73b4b29d390024a22f0d315a5cd1c496
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-07-15 11:07:10 -0700

    pushing up template

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+var http = require('http').createServer(app)
+const path = require('path')
+const router = express.Router()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/view'))
+app.use('/', router)
+
+
+app.get('/', (request, response) => {
+	response.status(200);
+	response.sendFile('view/index.html', {root: __dirname});
+})
+
+app.get('/formResults', (request, response) => {
+	response.status(200);
+	response.sendFile('view/formResults.html', {root : __dirname})
+})
+
+
+http.listen(PORT, () => {
+	console.log(`Server is up and listening on ${PORT}`)
+})
